2ded33bbdd9db521ae4d9e261fca533a0085b16f
awago0819 <o.11123456789999.o@gmail.com>
2023-08-19 23:29:44 +0900

    'commit'

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json'); //[env];
 const db = {};
 
 
@@ -25,12 +25,12 @@ const db = new Client({
 db.connect();
  */
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+//let sequelize;
+//if (config.use_env_variable) {
+//  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+//} else {
+ let sequelize = new Sequelize(config.database, config.username, config.password, config);
+//}
 
 fs
   .readdirSync(__dirname)
